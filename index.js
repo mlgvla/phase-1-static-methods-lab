a24d95f91abc39d687938f214a631644f342ddd0
mlgvla <mlgerardvla@gmail.com>
2022-01-14 02:10:13 -0500

    solution

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string[0].toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9 '-]/g, "");
+  }
+
+  static titleize(string) {
+    let wordArr = string.split(" ");
+    let noWordsArr = ["a", "an", "but", "of", "and", "for", "at", "by", "from", "the"];
+
+    for (let i = 0; i < wordArr.length; i++) {
+      if (!noWordsArr.includes(wordArr[i])) {
+        wordArr[i] = this.capitalize(wordArr[i]);
+      }
+    }
+    return this.capitalize(wordArr.join(" "));
+  }
 }
